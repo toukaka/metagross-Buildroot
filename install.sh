cce3b03baae3e4e6b11f6c570d63da2db6401b91
mohamed toumi <toumi.mednour@gmail.com>
2023-04-18 03:23:15 +0200

    update defconfig

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 yes | cp output/images/am335x-boneblack.dtb output/images/zImage /srv/tftp/ &&  rm -rvf ../rootfs_nfs/* && tar -xvf output/images/rootfs.tar -C ../rootfs_nfs/  && echo "INSTALL SUCCESS"
- make linux-update-defconfig && make savedefconfig
+
+
+make linux-update-defconfig && make savedefconfig
+
+
+make linux-rebuild && make -j16
